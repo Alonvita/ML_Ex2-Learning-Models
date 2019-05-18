55790037e601433db9a55ca1f07638bfb8f74be0
root <root@DESKTOP-U49BLG5.localdomain>
2019-05-19 00:15:33 +0300

    finished the PA algorithm

--- ModelInterface.py
+++ ModelInterface.py
@@ -1,7 +1,7 @@
 # Created By Alon Vita
 class Model(object):
-    def predict(self, x):
+    def predict(self, row):
         raise NotImplementedError
 
-    def update(self, x, y, learn_rate):
+    def update(self, row, row_y_values, learn_rate):
         raise NotImplementedError
