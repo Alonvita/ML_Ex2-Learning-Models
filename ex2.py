2fd7294be63c2c8c145f95f86768a4baa1622afc
root <root@DESKTOP-U49BLG5.localdomain>
2019-05-18 17:24:06 +0300

    updated loading df func

--- ex2.py
+++ ex2.py
@@ -3,8 +3,5 @@ import random
 from TrainingSetManipulations import TrainingSetManipulations as TSM
 
 if __name__ == "__main__":
-    df = TSM.train_fp_to_data_frame("train_x.txt")
-
-    print(df)
-
+    df = TSM.train_fp_to_data_frame("train_x.txt", "train_y.txt", True)
     print(df)
